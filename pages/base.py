91e2e608fbd93c1792e8847e93162bb2742a3b38
Catalina <44349388+CatalinaBrunchi@users.noreply.github.com>
2024-12-05 17:41:47 -0800

    Removed changes in base.py because of Merge conflict

--- pages/base.py
+++ pages/base.py
@@ -22,12 +22,3 @@ class BasePage:
 
     def open_page_devices(self):
         self.locate_element(self.page_devices).click()
-
-    def open_page_records(self):
-        self.locate_element(self.page_records).click()
-
-    def open_page_logbook(self):
-        self.locate_element(self.page_logbook).click()
-
-    def logout(self):
-        self.locate_element(self.button_logout).click()
