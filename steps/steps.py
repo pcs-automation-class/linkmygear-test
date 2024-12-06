9bc1cd4a92c880de9c26b2e8faa58b03c508b2d8
Andrey <baykov.andrey@gmail.com>
2024-12-05 23:06:00 -0500

    Dec 5

---
+++ steps/steps.py
@@ -0,0 +1,17 @@
+from time import sleep
+
+from behave import step
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+@step('Open "{env}" environment')
+def open_url(context, env):
+    environments = {
+        "dev": "https://test:FjeKB9ySMzwvDUs2XACpfu@dev.linkmygear.com",
+        "prod": "https://app.linkmygear.com",
+    }
+
+    context.driver.get(environments[env])
