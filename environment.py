806dac807dabd2d72bf68778749d3e973588eba6
pcs-automation-class <159883959+pcs-automation-class@users.noreply.github.com>
2024-12-10 18:24:10 -0500

    Update environment.py

--- environment.py
+++ environment.py
@@ -18,31 +18,31 @@ def before_all(context):
     with open("setting.json", "r") as file:
         file_data = json.load(file)
         context.credentials = file_data["Users"]
-        context.DeviceSettings = file_data["Project_settings"]
+        context.settings = file_data["Project_settings"]
 
 
 def before_scenario(context, scenario):
     if "EMULATE" not in scenario.name:
-        if context.DeviceSettings["browser"] == "Chrome":
+        if context.settings["browser"] == "Chrome":
             chrome_options = ChromeOptions()
             context.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),
                                               options=chrome_options)
-        elif context.DeviceSettings["browser"] == "Firefox":
+        elif context.settings["browser"] == "Firefox":
             firefox_options = FirefoxOptions()
             context.driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()),
                                                options=firefox_options)
         else:
             assert False, "Unknown browser!"
 
-        if context.DeviceSettings["maximize_window"]:  # Same as context.DeviceSettings["maximize_window"] is True
+        if context.settings["maximize_window"]:  # Same as context.DeviceSettings["maximize_window"] is True
             context.driver.maximize_window()
         else:
-            if context.DeviceSettings["screens"]["iPhone 14 Pro Max"]["portrait"]:
-                screen_width = context.DeviceSettings["screens"]["iPhone 14 Pro Max"]["screen_width"]
-                screen_height = context.DeviceSettings["screens"]["iPhone 14 Pro Max"]["screen_height"]
+            if context.settings["screens"]["iPhone 14 Pro Max"]["portrait"]:
+                screen_width = context.settings["screens"]["iPhone 14 Pro Max"]["screen_width"]
+                screen_height = context.settings["screens"]["iPhone 14 Pro Max"]["screen_height"]
             else:
-                screen_height = context.DeviceSettings["screens"]["iPhone 14 Pro Max"]["screen_width"]
-                screen_width = context.DeviceSettings["screens"]["iPhone 14 Pro Max"]["screen_height"]
+                screen_height = context.settings["screens"]["iPhone 14 Pro Max"]["screen_width"]
+                screen_width = context.settings["screens"]["iPhone 14 Pro Max"]["screen_height"]
 
             context.driver.set_window_size(screen_width, screen_height)
 
