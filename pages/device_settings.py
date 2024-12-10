aa089f95463e7a5e379a9e5bb51b5a1f3b897d76
Anna Vaysman <avaysman2000@yahoo.com>
2024-12-09 21:31:31 -0500

    Update Device Name

--- pages/device_settings.py
+++ pages/device_settings.py
@@ -4,15 +4,9 @@ from base import BasePage
 
 
 class DeviceSettings(BasePage):
-    def __init__(self):
-        super().__init__()
-        self.header = (By.XPATH, "")  # Device Settings
-
-
-    def __init__(self):
-        super().__init__()
-        self.page_header = (By.XPATH, "//h5[text()='Login to Your Account']")
-        self.email = (By.XPATH, "")
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.page_header = (By.XPATH, "//h5[text()='Login to Your Account']")  # Device Settings
 
     def verify_page(self):
-        super().verify_page(self.header)
+        super().verify_page(self.page_header)
